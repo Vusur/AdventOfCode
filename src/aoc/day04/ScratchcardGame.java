b2a86cd7bb6f10a8241e0f51659d94738e7e9b7c
Marosh <Marosh@ShinLi.fritz.box>
2023-12-04 19:16:12 +0100

    removed imports

--- src/aoc/day04/ScratchcardGame.java
+++ src/aoc/day04/ScratchcardGame.java
@@ -4,11 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ScratchcardGame {
