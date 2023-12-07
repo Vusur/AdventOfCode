78ed4b2c5b1ca418ed46ed9521a05e639b4d5169
Marosh <Marosh@ShinLi.fritz.box>
2023-12-07 19:22:45 +0100

    initial refactoring

--- src/aoc/day07/CamelPoker.java
+++ src/aoc/day07/CamelPoker.java
@@ -3,14 +3,13 @@ package aoc.day07;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class CamelPoker {
 
 	List<Hand> hands = new ArrayList<>();
-	List<Hand> HandJoker = new ArrayList<>();
+	List<Hand> handJoker = new ArrayList<>();
 	
 	public CamelPoker(String fpath) throws FileNotFoundException {
 		File input = new File(fpath);
@@ -18,24 +17,20 @@ public class CamelPoker {
 		//Hand hand = new Hand(sc.nextLine());
 		while(sc.hasNextLine()) {
 			var in = sc.nextLine();
-			hands.add(Hand.evaluate(in));
-			HandJoker.add(Hand.evaluateJoker(in));
+			hands.add(Hand.evaluate(in, false));
+			handJoker.add(Hand.evaluate(in, true));
 		}
 		sc.close();
 		hands.sort(Hand::compareTo);
-		HandJoker.sort(Hand::compareTo);
+		handJoker.sort(Hand::compareTo);
 		System.out.println("Part 1: " + totalWinnings(hands));
-		System.out.println("Part 2: " + totalWinnings(HandJoker));
+		System.out.println("Part 2: " + totalWinnings(handJoker));
 	}
 	
 	public int totalWinnings(List<Hand> h) {
 		int i = 1;
 		int result = 0;
 		for(Hand hand : h) {
-			System.out.print("Rank " + i + ": " );
-			System.out.print(hand.category().name() + "   ");
-			Arrays.stream(hand.cards()).forEach(c -> System.out.print(c.name() + " "));
-			System.out.println(" Bid: " + hand.bid());
 			result += hand.bid()*i;
 			i++;
 		}
--- src/aoc/day07/Hand.java
+++ src/aoc/day07/Hand.java
@@ -16,6 +16,7 @@ import java.util.stream.Stream;
 public record Hand(Category category, Integer bid, Card... cards) implements Comparable<Hand>{
 	
 	private static Map<String, Card> dict = Map.ofEntries(
+			Map.entry("X", JOKER),
 			Map.entry("2", TWO), 
 			Map.entry("3", THREE),
 			Map.entry("4", FOUR), 
@@ -30,29 +31,14 @@ public record Hand(Category category, Integer bid, Card... cards) implements Com
 			Map.entry("K", KING),
 			Map.entry("A", ACE)
 			);
-	
-	private static Map<String, Card> dictJoker = Map.ofEntries(
-			Map.entry("2", TWO), 
-			Map.entry("3", THREE),
-			Map.entry("4", FOUR), 
-			Map.entry("5", FIVE),
-			Map.entry("6", SIX), 
-			Map.entry("7", SEVEN),
-			Map.entry("8", EIGHT), 
-			Map.entry("9", NINE),
-			Map.entry("T", TEN), 
-			Map.entry("J", JOKER),
-			Map.entry("Q", QUEEN), 
-			Map.entry("K", KING),
-			Map.entry("A", ACE)
-			);
 
-	public static Hand evaluate(String input) {
+	public static Hand evaluate(String input, boolean joker) {
 		var temp = input.strip().split(" ");
-		var cards = getCards(temp[0]);
+		var cards = getCards(temp[0], joker);
 		var bid = Integer.parseInt(temp[1]);
 		
 		var counts =  Arrays.stream(cards).collect(groupingBy(card -> card, counting()));
+		counts = joker ? convertJoker(counts) : counts;
 		var ranks =	counts.entrySet().stream()
 				.sorted(comparing(Entry<Card, Long>::getValue).reversed())
 				.map(Entry::getKey)
@@ -69,32 +55,9 @@ public record Hand(Category category, Integer bid, Card... cards) implements Com
 		}
 	}
 	
-	private static Card[] getCards(String input) {	
+	private static Card[] getCards(String input, boolean joker) {
+		input = joker ? input.replace("J", "X") : input;
 		return Stream.of(input.split("")).map(in -> dict.get(in)).toArray(Card[]::new);
-	}
-	
-	//Part 2
-	public static Hand evaluateJoker(String input) {
-		var temp = input.strip().split(" ");
-		var cards = getCardsJoker(temp[0]);
-		var bid = Integer.parseInt(temp[1]);
-		
-		var counts =  Arrays.stream(cards).collect(groupingBy(card -> card, counting()));
-		counts = convertJoker(counts);
-		var ranks =	counts.entrySet().stream()
-				.sorted(comparing(Entry<Card, Long>::getValue).reversed())
-				.map(Entry::getKey)
-				.toArray(Card[]::new);
-		
-		if(ranks.length == 4) {
-			return new Hand(ONE_PAIR, bid, cards);
-		} else if(ranks.length == 3) {
-			return new Hand(counts.get(ranks[0]) == 2 ? TWO_PAIR : THREE_OF_A_KIND, bid, cards);
-		} else if(ranks.length == 2) {
-			return new Hand(counts.get(ranks[0]) == 3 ? FULL_HOUSE : FOUR_OF_A_KIND, bid, cards);
-		} else {
-			return new Hand(counts.get(ranks[0]) == 5 ? FIVE_OF_A_KIND : HIGH_CARD, bid, cards);
-		}
 	}
 	
 	private static Map<Card, Long> convertJoker(Map<Card, Long> current){
@@ -108,11 +71,6 @@ public record Hand(Category category, Integer bid, Card... cards) implements Com
 		}
 		return current;
 	}
-		
-	private static Card[] getCardsJoker(String input) {	
-		return Stream.of(input.split("")).map(in -> dictJoker.get(in)).toArray(Card[]::new);
-	}
-	
 	
 	@Override
 	public int compareTo(Hand other) {
